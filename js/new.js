72dbd7d46db001fb28fe69621204234894db43de
Sardor1009 <islomovsardor1009@gmail.com>
2023-12-02 19:45:34 +0500

    xh

---
+++ js/new.js
@@ -0,0 +1,9 @@
+let navbar = document.getElementById("navbar");
+  
+  function shrink() {
+    if (scrollY > 100) {
+      navbar.classList.add("navbar-shrink");
+    } else {
+          navbar.classList.remove("navbar-shrink");
+      }
+  }
